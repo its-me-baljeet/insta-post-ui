6d7dd512a6e3cbf790e64b691ff6181eb4451bb5
Baljeet Singh <bsbaljeetsingh9999@gmail.com>
2025-04-20 00:10:11 +0530

    Initialize posts with default data if localStorage is empty in Insta component

--- src/Insta.jsx
+++ src/Insta.jsx
@@ -2,7 +2,26 @@ import { useState } from "react";
 import PostSec from "./components/PostSec";
 
 const Insta = () => {
-    const initialPosts = JSON.parse(localStorage.getItem("postsList"));
+    const initialPosts = JSON.parse(localStorage.getItem("postsList")) || [
+        {
+            id: 1,
+            username: "im.baljeet_singh",
+            isLiked: false,
+            caption: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, quasi?"
+        },
+        {
+            id: 2,
+            username: "im.baljeet_singh",
+            isLiked: false,
+            caption: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, quasi?"
+        },
+        {
+            id: 3,
+            username: "im.baljeet_singh",
+            isLiked: false,
+            caption: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, quasi?"
+        },
+    ];
     const [posts, setPosts] = useState(initialPosts);
     return (
         <div className="h-[100vh] flex flex-col justify-start items-center gap-8 bg-[#191919] text-white">
